a607112fdc0ee344e3a0814ab55a19e768230a68
luuizfernando <luizfernandosant26@gmail.com>
2026-04-09 12:31:27 -0300

    project created

---
+++ src/test/java/com/luiz/transactions/TransactionsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.luiz.transactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
