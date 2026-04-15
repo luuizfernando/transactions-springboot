1cf51d0b01f8896d322710c272a5835579df5a62
luuizfernando <luizfernandosant26@gmail.com>
2026-04-14 22:08:40 -0300

    feat(security): criar autenticação básica para login

---
+++ src/main/java/com/luiz/transactions/config/security/SecurityFilter.java
@@ -0,0 +1,54 @@
+package com.luiz.transactions.config.security;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.luiz.transactions.repository.UserRepository;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private final TokenService tokenService;
+    private final UserRepository userRepository;
+
+    public SecurityFilter(TokenService tokenService, UserRepository userRepository) {
+        this.tokenService = tokenService;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+                var token = this.recoverToken(request);
+
+                if (token != null) {
+                    try {
+                        var subject = tokenService.validateToken(token);
+                        UserDetails user = userRepository.findByName(subject);
+                        var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                        SecurityContextHolder.getContext().setAuthentication(authentication);
+                    } catch (Exception e) {
+                        System.out.println("Erro no SecurityFilter: " + e.getMessage());
+                    }
+                }
+
+                filterChain.doFilter(request, response);
+            }
+
+    private String recoverToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
+    }
+    
+}
---
+++ src/main/java/com/luiz/transactions/service/AuthorizationService.java
@@ -0,0 +1,24 @@
+package com.luiz.transactions.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.luiz.transactions.repository.UserRepository;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public AuthorizationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String name) throws UsernameNotFoundException {
+        return userRepository.findByName(name);
+    }
+    
+}
