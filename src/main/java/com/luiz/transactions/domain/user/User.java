8d50c4d931e4423b1b5c9c39e7ccac0103c0eb87
luuizfernando <luizfernandosant26@gmail.com>
2026-04-14 22:11:33 -0300

    feat(User): implement UserDetails interface and add role-based authority management

--- src/main/java/com/luiz/transactions/domain/user/User.java
+++ src/main/java/com/luiz/transactions/domain/user/User.java
@@ -1,11 +1,22 @@
 package com.luiz.transactions.domain.user;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import com.luiz.transactions.domain.account.Account;
+import com.luiz.transactions.domain.user.enums.UserRole;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,13 +26,18 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     private String name;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role = UserRole.USER;
 
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private Account account;
@@ -30,8 +46,10 @@ public class User {
 
     }
 
-    public User(String name) {
+    public User(String name, String password, UserRole role) {
         this.name = name;
+        this.password = password;
+        this.role = role;
         this.account = new Account(this);
     }
 
@@ -53,6 +71,45 @@ public class User {
 
     public Account getAccount() {
         return account;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (role == UserRole.ADMIN) return List.of(
+            new SimpleGrantedAuthority("ROLE_ADMIN"),
+            new SimpleGrantedAuthority("ROLE_USER")
+        );
+        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return name;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
 }
--- src/main/java/com/luiz/transactions/repository/UserRepository.java
+++ src/main/java/com/luiz/transactions/repository/UserRepository.java
@@ -3,11 +3,14 @@ package com.luiz.transactions.repository;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.luiz.transactions.domain.user.User;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
     boolean existsByName(String name);
+
+    UserDetails findByName(String name);
 
 }
