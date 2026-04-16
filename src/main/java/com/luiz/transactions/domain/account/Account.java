3bbfb6d5ac8ea357800e4c538837e82f5c00b919
luuizfernando <luizfernandosant26@gmail.com>
2026-04-15 21:21:25 -0300

    refactor(Account): remover id compartilhado com usuário

--- src/main/java/com/luiz/transactions/domain/account/Account.java
+++ src/main/java/com/luiz/transactions/domain/account/Account.java
@@ -8,9 +8,10 @@ import com.luiz.transactions.domain.user.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
@@ -19,11 +20,11 @@ import jakarta.persistence.Table;
 public class Account {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @OneToOne(fetch = FetchType.LAZY)
-    @MapsId
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
     private User user;
 
     @Column(nullable = false, precision = 19, scale = 2)
@@ -31,11 +32,6 @@ public class Account {
 
     public Account() {
 
-    }
-
-    public Account(UUID id, User user) {
-        this.id = id;
-        this.user = user;
     }
 
     public Account(User user) {
