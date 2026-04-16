3b5a4c86062ec5a4702983814e6f074e0571370b
luuizfernando <luizfernandosant26@gmail.com>
2026-04-16 10:42:49 -0300

    refactor(Account): criar conta para usuário ao se registrar

--- src/main/java/com/luiz/transactions/service/AccountService.java
+++ src/main/java/com/luiz/transactions/service/AccountService.java
@@ -7,29 +7,22 @@ import com.luiz.transactions.domain.account.Account;
 import com.luiz.transactions.domain.account.dto.AccountResponseDTO;
 import com.luiz.transactions.domain.user.User;
 import com.luiz.transactions.exception.ConflictException;
-import com.luiz.transactions.exception.ResourceNotFoundException;
 import com.luiz.transactions.repository.AccountRepository;
-import com.luiz.transactions.repository.UserRepository;
 
 @Service
 public class AccountService {
 
     private final AccountRepository accountRepository;
-    private final UserRepository userRepository;
 
-    public AccountService(AccountRepository accountRepository, UserRepository userRepository) {
+    public AccountService(AccountRepository accountRepository) {
         this.accountRepository = accountRepository;
-        this.userRepository = userRepository;
     }
 
     /**
      * Cria uma conta vinculada ao usuário autenticado. Cada usuário pode ter no máximo uma conta.
      */
     @Transactional
-    public AccountResponseDTO createAccountForUser(User principal) {
-        User user = userRepository.findById(principal.getId())
-                .orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado."));
-
+    public AccountResponseDTO createAccountForUser(User user) {
         if (accountRepository.existsByUserId(user.getId())) {
             throw new ConflictException("Este usuário já possui uma conta.");
         }
@@ -37,7 +30,6 @@ public class AccountService {
         Account account = new Account(user);
         account = accountRepository.save(account);
         user.setAccount(account);
-        userRepository.save(user);
 
         return new AccountResponseDTO(account.getId(), user.getId());
     }
